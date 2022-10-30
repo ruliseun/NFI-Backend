d4103c58230437d4b2ec82af44be2ec56fdd7c9b
Oluwaseun <ruliseun@gmail.com>
2022-10-30 23:15:23 +0100

    clean up

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import express, {Request, Response, NextFunction} from 'express';
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.status(200).json({message: 'Welcome to NFI Backend test project'})
 });
